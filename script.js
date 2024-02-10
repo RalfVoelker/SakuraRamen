05de527821ecacd8ca45a60f2edf4f8da2d35db2
Ralf Voelker <ralf.voelker@web-technologie-service.de>
2024-02-11 00:24:05 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+
+function changeToGerman() {
+    document.getElementById('a-menu').innerHTML='MENÜ';
+    document.getElementById('a-adress').innerHTML='STANDORT';
+    document.getElementById('a-about').innerHTML='ÜBER';
+    document.getElementById('info-hero').innerHTML='DIE BESTEN RAMEN DER STADT';
+    document.getElementById('how_to_order').innerHTML='WIE MAN BESTELLT';
+    document.getElementById('hto-1').innerHTML='Wähle deine ';
+    document.getElementById('hto-2').innerHTML='Nudeln';
+    document.getElementById('hto-3').innerHTML='Wähle dein';
+    document.getElementById('hto-4').innerHTML='Brot';
+    document.getElementById('hto-5').innerHTML='Wähle deine';
+    document.getElementById('hto-6').innerHTML='Beilage';
+    document.getElementById('menu').innerHTML='UNSER MENÜ';
+    document.getElementById('tonkotsu-c').innerHTML='Huhn';
+    document.getElementById('tonkotsu-b').innerHTML='Rind';
+    document.getElementById('tonkotsu-s').innerHTML='Schrimps';
+    document.getElementById('SpicyMiso-c').innerHTML='Huhn';
+    document.getElementById('SpicyMiso-b').innerHTML='Rind';
+    document.getElementById('SpicyMiso-s').innerHTML='Schrimps';
+    document.getElementById('SpicyMiso-c2').innerHTML='Huhn';
+    document.getElementById('SpicyMiso-b2').innerHTML='Rind';
+    document.getElementById('SpicyMiso-s2').innerHTML='Schrimps';
+    document.getElementById('shio-c').innerHTML='Huhn';
+    document.getElementById('shio-b').innerHTML='Rind';
+    document.getElementById('shio-s').innerHTML='Schrimps';
+    document.getElementById('adress').innerHTML='SO FINDEN SIE UNS';
+}
